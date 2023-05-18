5ba23a154e5862c0bf3ae55bff06d6843dcba494
Soorya2811 <129177818+Soorya2811@users.noreply.github.com>
2023-05-18 14:17:08 +0530

    Create question 26.c

---
+++ question 26.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int max = 0;
+    int num;
+
+    printf("Enter the total number of integers: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        printf("Enter integer %d: ", i);
+        scanf("%d", &num);
+
+        if (num > max) {
+            max = num;
+        }
+    }
+
+    printf("The biggest number is: %d\n", max);
+
+    return 0;
+}
+
+OUTPUT 
+Enter the total number of integers: 3
+Enter integer 1: 5
+Enter integer 2: 7
+Enter integer 3: 8
+The biggest number is: 8
